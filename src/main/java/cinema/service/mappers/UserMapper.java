23ff9b8444d3f85a438d482987b991736ae3f629
OleksandrPokotylenko <aln.nko.of@gmail.com>
2021-02-22 16:32:00 +0200

    deleted unnecessary injection in UserMapper class

--- src/main/java/cinema/service/mappers/UserMapper.java
+++ src/main/java/cinema/service/mappers/UserMapper.java
@@ -2,19 +2,10 @@ package cinema.service.mappers;
 
 import cinema.model.User;
 import cinema.model.dto.UserResponseDto;
-import cinema.service.RoleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
-    private final RoleService roleService;
-
-    @Autowired
-    public UserMapper(RoleService roleService) {
-        this.roleService = roleService;
-    }
-
     public UserResponseDto mapToDto(User user) {
         UserResponseDto userResponseDto = new UserResponseDto();
         userResponseDto.setId(user.getId());
